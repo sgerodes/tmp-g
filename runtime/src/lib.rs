c21ec5752ab4d8c644a627f52262920a25256b9c
sgerodes <sgerodes@gmail.com>
2024-05-27 20:15:24 +0200

    feat(runtime): adds main pallets

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -282,6 +282,90 @@ impl pallet_identity::Config for Runtime {
 	type WeightInfo = pallet_identity::weights::SubstrateWeight<Runtime>;
 
 }
+
+parameter_types! {
+	pub const ProposalBond: Permill = Permill::from_percent(5);
+	pub const ProposalBondMinimum: Balance = 1 * DOLLARS;
+	pub const SpendPeriod: BlockNumber = 7 * DAYS;
+	pub const Burn: Permill = Permill::from_percent(1);
+	pub const DataDepositPerByte: Balance = 1 * CENTS;
+	pub const MaximumReasonLength: u32 = 16384;
+	pub const SpendFundsPeriod: BlockNumber = 24 * HOURS;
+}
+
+impl pallet_treasury::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type ApproveOrigin = EnsureRoot<AccountId>;
+	type RejectOrigin = EnsureRoot<AccountId>;
+	type ProposalBond = ProposalBond;
+	type ProposalBondMinimum = ProposalBondMinimum;
+	type SpendPeriod = SpendPeriod;
+	type Burn = Burn;
+	type PalletId = frame_support::PalletId(*b"py/trsry");
+	type BurnDestination = ();
+	type SpendFundsPeriod = SpendFundsPeriod;
+	type DataDepositPerByte = DataDepositPerByte;
+	type MaximumReasonLength = MaximumReasonLength;
+	type WeightInfo = pallet_treasury::weights::SubstrateWeight<Runtime>;
+	type SpendFunds = ();
+}
+
+
+//
+// parameter_types! {
+// 	pub const LaunchPeriod: BlockNumber = 5 * DAYS;
+// 	pub const VotingPeriod: BlockNumber = 5 * DAYS;
+// 	pub const FastTrackVotingPeriod: BlockNumber = 3 * HOURS;
+// 	pub MinimumDeposit: Balance = 1000;
+// 	pub const EnactmentPeriod: BlockNumber = 2 * DAYS;
+// 	pub const VoteLockingPeriod: BlockNumber = 14 * DAYS;
+// 	pub const CooloffPeriod: BlockNumber = 7 * DAYS;
+// }
+//
+// impl pallet_democracy::Config for Runtime {
+// 	type RuntimeEvent = RuntimeEvent;
+// 	type Currency = Balances;
+// 	type EnactmentPeriod = EnactmentPeriod;
+// 	type LaunchPeriod = LaunchPeriod;
+// 	type VotingPeriod = VotingPeriod;
+// 	type VoteLockingPeriod = VoteLockingPeriod;
+// 	type MinimumDeposit = MinimumDeposit;
+// 	/// A straight majority of the council can decide what their next motion is.
+// 	type ExternalOrigin = EnsureRootOrHalfGeneralCouncil;
+// 	/// A majority can have the next scheduled referendum be a straight majority-carries vote.
+// 	type ExternalMajorityOrigin = EnsureRootOrHalfGeneralCouncil;
+// 	/// A unanimous council can have the next scheduled referendum be a straight default-carries
+// 	/// (NTB) vote.
+// 	type ExternalDefaultOrigin = EnsureRootOrAllGeneralCouncil;
+// 	/// Two thirds of the technical committee can have an ExternalMajority/ExternalDefault vote
+// 	/// be tabled immediately and with a shorter voting/enactment period.
+// 	type FastTrackOrigin = EnsureRootOrTwoThirdsTechnicalCommittee;
+// 	type InstantOrigin = EnsureRootOrAllTechnicalCommittee;
+// 	type InstantAllowed = ConstBool<true>;
+// 	type FastTrackVotingPeriod = FastTrackVotingPeriod;
+// 	// To cancel a proposal which has been passed, 2/3 of the council must agree to it.
+// 	type CancellationOrigin = EnsureRootOrTwoThirdsGeneralCouncil;
+// 	type BlacklistOrigin = EnsureRoot<AccountId>;
+// 	// To cancel a proposal before it has been passed, the technical committee must be unanimous or
+// 	// Root must agree.
+// 	type CancelProposalOrigin = EnsureRootOrAllTechnicalCommittee;
+// 	// Any single technical committee member may veto a coming council proposal, however they can
+// 	// only do it once and it lasts only for the cooloff period.
+// 	type VetoOrigin = pallet_collective::EnsureMember<AccountId, TechnicalCommitteeInstance>;
+// 	type CooloffPeriod = CooloffPeriod;
+// 	type Slash = Treasury;
+// 	type Scheduler = Scheduler;
+// 	type PalletsOrigin = OriginCaller;
+// 	type MaxVotes = ConstU32<100>;
+// 	type WeightInfo = pallet_democracy::weights::SubstrateWeight<Runtime>;
+// 	type MaxProposals = ConstU32<100>;
+// 	type Preimages = Preimage;
+// 	type MaxDeposits = ConstU32<100>;
+// 	type MaxBlacklisted = ConstU32<100>;
+// 	type SubmitOrigin = EnsureSigned<AccountId>;
+// }
+
 //
 // parameter_types! {
 // 	pub const CouncilMotionDuration: BlockNumber = 30 * MINUTES;
@@ -337,6 +421,7 @@ construct_runtime!(
 		TemplateModule: pallet_template,
 
 		Identity: pallet_identity,
+		Treasury: pallet_treasury,
 		// Collective: pallet_collective,
 		// Membership: pallet_membership,
 	}

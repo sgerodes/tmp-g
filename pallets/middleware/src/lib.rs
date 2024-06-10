1f920922e18abbb3af220fe07c31bba130550ade
sgerodes <sgerodes@gmail.com>
2024-06-10 15:15:41 +0200

    feat(middleware): implements middleware initial structure

---
+++ pallets/middleware/src/lib.rs
@@ -0,0 +1,48 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+pub mod weights;
+pub use weights::*;
+
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        type WeightInfo: WeightInfo;
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        DummyEvent
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        DummyError
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(T::WeightInfo::do_something())]
+        pub fn do_something(origin: OriginFor<T>) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
+            Self::deposit_event(Event::DummyEvent);
+            Ok(())
+        }
+
+    }
+}
